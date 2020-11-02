9fd20ee39dd89273cc8cc0b322ff5f4272838084
AlaaDq <45366622+AlaaDq@users.noreply.github.com>
2020-11-02 20:57:05 +0200

    final code

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,5 +1,5 @@
   
-import { ActionReducer, ActionReducerMap, MetaReducer, createSelector } from '@ngrx/store'
+import {  ActionReducerMap, createSelector } from '@ngrx/store'
 
 // import { environment } from '../../environments/environment'
 import * as fromSearch from './search.reducer'
@@ -27,17 +27,4 @@ export const selectCurrentWeather = createSelector(
   (current) => current
 )
 
-// export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
-//   return (state, action) => {
-//     const result = reducer(state, action)
-//     console.groupCollapsed(action.type)
-//     console.log('prev state', state)
-//     console.log('action', action)
-//     console.log('next state', result)
-//     console.groupEnd()
 
-//     return result
-//   }
-// }
-
-// export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : []
