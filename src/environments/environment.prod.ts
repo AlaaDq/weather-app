975da54dbf020ba2f1c67a0b62cbcbb2e0552fa6
AlaaDq <45366622+AlaaDq@users.noreply.github.com>
2020-12-06 17:01:53 +0200

    production env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+
+  appName:'default-application_4825027',    
+  xRapidapiHost: "community-open-weather-map.p.rapidapi.com",
+  useQueryString: true,
+
+  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com',
+  xRapidapiKey: "38bb2106a6msh093db34839d020ep1491ebjsn2718bc7167a0",
 };
